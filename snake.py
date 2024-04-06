7fc14be0939f9d7a65d0655266da0dac82bbf539
Naufal M <naufalnihal10@gmail.com>
2024-04-06 12:32:01 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -26,9 +27,6 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
-
-
-
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
@@ -39,6 +37,14 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
 
     def down(self):
         if self.head.heading() != UP:
