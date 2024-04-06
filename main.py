9e235cc5bafeb710dee0c82d5b761d5ed381b7bd
Naufal M <naufalnihal10@gmail.com>
2024-04-06 12:30:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
+
 screen = Screen()
 screen.setup(600,600)
 screen.bgcolor("black")
@@ -15,40 +16,36 @@ scoreboard = Scoreboard()
 
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.05)
 
-    #Snake move
-
+    #this line snake move.
     snake.move()
 
-    #Scoreboard
-
+    #this if-statement the calculates score.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with walls
-
+    #collision with walls.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    #Detect collision with tail.
-
+    #collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset()
 
 
 
